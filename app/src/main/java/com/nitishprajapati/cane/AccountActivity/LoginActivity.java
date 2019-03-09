53aa6f251dbbca9ced9b119904c4bbed866a4f52
nitishprajapati <prajapati.nitish.np@gmail.com>
2019-03-09 20:24:03 +0530

    Initial commit

---
+++ app/src/main/java/com/nitishprajapati/cane/AccountActivity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.nitishprajapati.cane.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nitishprajapati.cane.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/nitishprajapati/cane/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nitishprajapati.cane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
